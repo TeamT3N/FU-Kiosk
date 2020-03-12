6ad97c0dd4581b0d8c78b147760780c066edd3f9
emmandesu <61533468+emmandesu@users.noreply.github.com>
2020-03-12 09:25:48 +0800

    Create ProfileViewModel.java

---
+++ app/src/main/java/com/TeamT3N/fukiosk/ui/Profile/ProfileViewModel.java
@@ -0,0 +1,18 @@
+package com.TeamT3N.fukiosk.ui.Profile;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ProfileViewModel extends ViewModel {
+    private static MutableLiveData<String> mText;
+
+    public ProfileViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is profile fragment");
+    }
+
+    public static LiveData<String> getText() {
+        return mText;
+    }
+}
