6c91b9edc6153fc1d044d23770aea5f0421b7929
emmandesu <61533468+emmandesu@users.noreply.github.com>
2020-03-12 09:23:24 +0800

    Create capstone.java

---
+++ app/src/main/java/com/TeamT3N/fukiosk/ui/ProfileOptions/Classes/capstone.java
@@ -0,0 +1,20 @@
+package com.TeamT3N.fukiosk.ui.Profile.ProfileOptions.Classes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.TeamT3N.fukiosk.R;
+
+public class capstone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_major);
+
+
+
+
+    }
+}
